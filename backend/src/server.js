8d2baff5eb95b064d148f45552ed48d4dd2f920e
Devansh <devansh14vashisht@gmail.com>
2026-03-22 15:26:40 +0530

    resolved deployment error on path and devdependecies

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,8 +20,9 @@ app.get('/', (req, res) => {
     res.send("express working");
 });
 
+// for deployment
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static(path.join(_dirname,"../frontend/dist")))
+    app.use(express.static(path.join(_dirname,"frontend/dist")))
 
     app.get("*",(req,res) =>{
         res.sendFile(path.join(_dirname,"../frontend","dist","index.html"))
